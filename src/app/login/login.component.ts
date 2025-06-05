9fd13bc8ac136bde84f6e71594ecebcc46ee4ef5
Abhishek Narwariya <abhisheknarwariya2804@gmail.com>
2025-06-05 19:38:37 +0530

    adding login signup code

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,63 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
+  ],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  signupForm: FormGroup;
+  loginForm: FormGroup;
+  showLogin = true;
+
+  constructor(private fb: FormBuilder) {
+    this.signupForm = this.fb.group({
+      username: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.signupForm.valid) {
+      // Handle signup logic here
+      console.log(this.signupForm.value);
+    }
+  }
+
+  onLogin() {
+    if (this.loginForm.valid) {
+      // Handle login logic here
+      console.log(this.loginForm.value);
+    }
+  }
+
+  openLogin() {
+    this.showLogin = true;
+  }
+
+  openSignup() {
+    this.showLogin = false;
+  }
+}
